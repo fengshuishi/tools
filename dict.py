c68ec5f705adddb8903666ab3eae1473a8680dd4
fengshuishi <42765345+fengshuishi@users.noreply.github.com>
2022-10-20 11:06:03 +0800

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,10 +1,8 @@
 def dict():
-    dict = open("/Users/cunyang/Desktop/tools/zidian/user.txt", "r",encoding= u'utf-8',errors='ignore')
-    # dict_list=list(dict.readlines())
+    dict = open("xxx/xxx/xxx/user.txt", "r",encoding= u'utf-8',errors='ignore')
+    # 读取本地爆破字典
     dict_list = dict.read().splitlines()
-    # print(dict_list)
-    # for i in range(len(dict_list)):
-    #     password =dict_list[i]
+   
     return dict_list
     dict.close()
 pw=dict()
